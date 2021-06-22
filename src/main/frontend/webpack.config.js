58ceff8fcc1e4df9d9d4436e4b6d4241b4acc3e7
benjamin-z-l <82854993+benjamin-z-l@users.noreply.github.com>
2021-06-22 18:06:06 -0400

    Update webpack.config.js
    Reverted this change. I was mistaken about directory structure

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    path: "../client/main.js",
+    path: "./client/main.js",
   },
   output: {
     path: path.join(__dirname, "../../../target/classes/static"),
