5b0f78a7173d8998ed29a08342b38ce75d330955
Suey Chen <suey.chen.16@gmail.com>
2021-06-22 15:25:05 -0400

    added links to show page on image and name on trail Index

--- src/main/frontend/client/components/TrailTile.js
+++ src/main/frontend/client/components/TrailTile.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TrailTile = props =>{
-  const {name, imgUrl, distance, difficulty, zipCode} = props.trail
+  const {id, name, imgUrl, distance, difficulty, zipCode} = props.trail
 
   return(
     <div className="grid-x grid-margin-x ">
       <div className="cell small-12 medium-6 large-6">
-        <img src={imgUrl}></img>
+        <Link to={`/trails/${id}`} >
+          <img src={imgUrl}></img>
+        </Link>
       </div>
       <div className="callout secondary cell small-12 medium-6 large-4">
-        <h3>{name}</h3>
+        <h3><Link to={`/trails/${id}`} >{name}</Link></h3>
         <h4>Difficulty: {difficulty}</h4>
         <h4>Distance: {distance}</h4>
         <h4>Location: {zipCode}</h4>
