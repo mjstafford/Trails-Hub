e3eb29c5ad7ee9c12e8fb59316da05ed5ea8ecb0
benjamin-z-l <you@example.com>
2021-06-22 12:03:46 -0400

    added navbar and router

--- src/main/frontend/components/App.js
+++ src/main/frontend/components/App.js
@@ -3,12 +3,25 @@ import { hot } from "react-hot-loader/root";
 import "foundation-sites";
 import $ from "jquery";
 
+import { Switch, Redirect, Route, BrowserRouter } from "react-router-dom";
+
+import NavBar from './NavBar';
+
 const App = (props) => {
   useEffect(() => {
     $(document).foundation();
   }, []);
 
-  return <h1>Hello from the frontend!</h1>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/trails" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default hot(App);
