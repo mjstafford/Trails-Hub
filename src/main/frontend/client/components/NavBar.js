04043c79b7802d71fe15aa85ec82d5b2fedc63bb
benjamin-z-l <you@example.com>
2021-06-22 22:13:24 -0400

    align top bar nav with center content

--- src/main/frontend/client/components/NavBar.js
+++ src/main/frontend/client/components/NavBar.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <Link to="/trails">Trails Hub</Link>
-        </ul>
-      </div>
+    <div className="top-bar grid-x">
+      <div className="cell auto"></div>
+        <div className="cell small-12 medium-8">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <Link to="/trails">Trails Hub</Link>
+              <Link to="/trails">Another thing</Link>
+            </ul>
+          </div>
+        </div>
+      <div className="cell auto"></div>
     </div>
   );
 };
