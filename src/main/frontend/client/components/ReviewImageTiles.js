083408e5f205410014c8b284ff956d2dd910dbe2
benjamin-z-l <you@example.com>
2021-06-24 15:44:42 -0400

    added Review Tile and ReviewImageTile

---
+++ src/main/frontend/client/components/ReviewImageTiles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewImageTile = props => {
+  return (
+    <div className="thumbnail">
+      <img src={} alt="reviewer image" />
+    </div>
+  );
+};
+
+export default ReviewImageTile;
